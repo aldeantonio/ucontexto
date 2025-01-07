f11df85ebcede9adf97eb080e2b49ed367dbd126
Alde Antonio <aldeantonio8@gmail.com>
2025-01-07 13:47:43 +0200

    Add class feed-card-author-inf

--- src/App.js
+++ src/App.js
@@ -14,31 +14,43 @@ const newsData = [
     title: 'Notícia 1',
     description: 'Descrição breve da notícia 1.',
     image: noticia1,
+    author: 'Autor 1',
+    date: 'Data 1',
   },
   {
     title: 'Notícia 2',
     description: 'Descrição breve da notícia 2.',
     image: noticia2,
+    author: 'Autor 2',
+    date: 'Data 2',
   },
   {
     title: 'Notícia 3',
     description: 'Descrição breve da notícia 3.',
     image: noticia3,
+    author: 'Autor 3',
+    date: 'Data 3',
   },
   {
     title: 'Notícia 4',
     description: 'Descrição breve da notícia 4.',
     image: noticia1,
+    author: 'Autor 4',
+    date: 'Data 4',
   },
   {
     title: 'Notícia 5',
     description: 'Descrição breve da notícia 5.',
     image: noticia2,
+    author: 'Autor 5',
+    date: 'Data 5',
   },
   {
     title: 'Notícia 6',
     description: 'Descrição breve da notícia 6.',
     image: noticia3,
+    author: 'Autor 6',
+    date: 'Data 6',
   },
 ];
 
--- src/components/Feed.js
+++ src/components/Feed.js
@@ -10,6 +10,14 @@ const Feed = ({ news }) => {
           <div className="feed-card-content">
             <h3 className="feed-card-title">{item.title}</h3>
             <p className="feed-card-description">{item.description}</p>
+            <div className='feed-card-author'>
+                <img src={item.image} alt={item.title} className="feed-card-author-image" />
+                <div className='feed-card-author-info'>
+                    <span className="feed-card-author-name">{item.author}</span>
+                    <span className="feed-card-author-date">{item.date}</span>
+                </div>
+
+            </div>
           </div>
         </div>
       ))}
