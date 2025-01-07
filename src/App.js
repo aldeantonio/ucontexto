be3be1684147e18b319b4e06b0d4b5e70f5651f3
Alde Antonio <aldeantonio8@gmail.com>
2025-01-07 15:06:22 +0200

    I add higgligth section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+// App.js
 import React from 'react';
 import Header from './components/Header';
 import Feed from './components/Feed';
+import Spotlight from './components/Spotlight'; // Renomeado para refletir o componente
 import './App.css';
 
 // Importando imagens locais
@@ -8,10 +10,9 @@ import noticia1 from './assets/news-images/cover-ia.jpg';
 import noticia2 from './assets/news-images/cover-arte.jpg';
 import noticia3 from './assets/news-images/cover-negocio.jpg';
 
-
 const newsData = [
   {
-    title: 'O Renascimento Digital: Como a Arte Tradicional está Ganhando Vida no Mundo Virtul',
+    title: 'O Renascimento Digital: Como a Arte Tradicional está Ganhando Vida no Mundo Virtual',
     description: 'Este renascimento digital não é apenas uma nova forma de expressão; é também uma maneira de conectar culturas, ressignificar o valor da arte tradicional e torná-la mais...',
     image: noticia1,
     author: 'Alde Ivaro',
@@ -33,20 +34,36 @@ const newsData = [
   },
 ];
 
+const spotlightData = {
+  image: noticia1, // Substitua pela URL da sua imagem
+  title: 'Destaque do Dia',
+  summary: 'Esta é uma breve descrição da notícia ou artigo em destaque.',
+  author: 'Aldo Silva',
+  date: '07 de Janeiro de 2025',
+};
+
 function App() {
   return (
     <div>
-      
       {/* Header no topo */}
       <Header />
 
       {/* Conteúdo principal */}
       <div className="container">
-      <main>
-        <h1>Ultimas Notícias</h1>
-        <Feed news={newsData} />
-      </main>
+        {/* Destaque do dia */}
+        <Spotlight
+          image={spotlightData.image}
+          title={spotlightData.title}
+          summary={spotlightData.summary}
+          author={spotlightData.author}
+          date={spotlightData.date}
+        />
 
+        {/* Últimas notícias */}
+        <main>
+          <h1>Últimas Notícias</h1>
+          <Feed news={newsData} />
+        </main>
       </div>
     </div>
   );
